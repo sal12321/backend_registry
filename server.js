4bfe5cc05052b278f81445c365aab51da5738ae3
sal12321 <aaqibalam290@gmail.com>
2026-09-06 18:36:34 +0530

    updating test route

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get("/api/testMongo", async (req, res) => {
 app.get("/api/test", (req, res)=>{
     res.json({
         "message" : "hey this is /api/test api",
-        "update" :  "this is simply an update 2.0"
+        "update" :  "this is simply an update 2.0 cicd"
     })
 })
 
